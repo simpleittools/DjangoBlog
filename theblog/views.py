606580ad58fc72c2b385daeceb82265cd6a37e8b
icedvigilante <wm.ryan.mooney@gmail.com>
2020-08-22 21:24:23 -0800

    bootstrap Add Post form

--- theblog/views.py
+++ theblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # def home(request):
@@ -10,6 +11,15 @@ class HomeView(ListView):
     model = Post
     template_name = "home.html"
 
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = "article_details.html"
+
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = '__all__'
+    # fields = ('title', 'title_tag', 'author', 'body')
