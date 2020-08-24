0ef900c58cc6ede5ea9724529b7013072e210323
icedvigilante <wm.ryan.mooney@gmail.com>
2020-08-23 16:05:26 -0800

    Setup initial user registration and authentication

--- simpleBlog/settings.py
+++ simpleBlog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'theblog',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
