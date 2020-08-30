2960d0172f22f65375947614144b17d6b2e7d233
icedvigilante <wm.ryan.mooney@gmail.com>
2020-08-29 20:04:47 -0800

    Setup Category Navbar

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 # from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     # path('', views.home, name='home')
@@ -10,7 +10,8 @@ urlpatterns = [
     path('add_category/', AddCategoryView.as_view(), name="add_category"),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name="update_post"),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name="delete_post"),
-    path('category/<str:cats>/', CategoryView, name='category')
+    path('category/<str:cats>/', CategoryView, name='category'),
+    path('category-list/', CategoryListView, name='category-list'),
 
 
 ]
