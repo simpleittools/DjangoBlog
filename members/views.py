599ca169e3a051c0573943edeb6d857c5f3c5b26
icedvigilante <wm.ryan.mooney@gmail.com>
2020-09-06 10:57:38 -0800

    setup retirect to create_profile_page after registration

--- members/views.py
+++ members/views.py
@@ -4,13 +4,21 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm, Password
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditSettingsForm, PasswordChangingForm, ProfilePageForm
 from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth import authenticate, login
 from django.views.generic import DetailView, CreateView
+from django.shortcuts import redirect
 from theblog.models import Profile
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
-
+    def form_valid(self, form):
+        form.save()
+        username = self.request.POST['username']
+        password = self.request.POST['password1']
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'], )
+        login(self.request, user)
+        return redirect(self.success_url)
     success_url = reverse_lazy('create_profile_page')
 
 
