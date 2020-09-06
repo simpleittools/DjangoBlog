f534a71be7959cc6688464497919a9ec1a8e4dde
icedvigilante <wm.ryan.mooney@gmail.com>
2020-09-06 12:57:59 -0800

    setup comment section - no form yet

--- theblog/models.py
+++ theblog/models.py
@@ -55,3 +55,15 @@ class Post(models.Model):
 
     def total_dislikes(self):
         return self.dislikes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    # body = models.TextField()
+    body = RichTextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+
